a07970b15919f2aae4c3d856be4f68a9a2b8f25a
KenNguch <kennguch3@gmail.com>
2021-10-02 20:59:52 +0300

    Rewrote the hello world

--- app.js
+++ app.js
@@ -2,30 +2,35 @@ const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv').config();
 const {ReasonPhrases, StatusCodes, getReasonPhrase, getStatusCode,} = require('http-status-codes');
-const {response} = require("express");
-
+const faker = require('faker');
 
 const app = express();
-
 const port = process.env.APP_PORT || 3000;
 const url = process.env.APP_URL || 'http://localhost';
-const prefix = process.env.APP_VERSION ;
 
-//Middleware
 app.use(morgan(`tiny`));
-app.use( `${prefix}`,router);
-
-
-
-
-
 
 app.get('/', (req, res, next) => {
-    res.send(port)
+    res.send( " Day 1 of expressjs #100DaysOfCode")
     res.status(StatusCodes.OK)
-
 })
 
+app.get('/products', (req, res, next) => {
+    const product = {
+        id: faker.datatype.number(),
+        name: faker.name.findName(),
+        image: faker.image.imageUrl(),
+        address: faker.address.streetAddress(),
+        employer: faker.company.companyName()
+    }
+    res.send(product)
+    res.status(StatusCodes.OK)
+})
+app.post(`/products`, (req, res) => {
+    const newProduct = req.body;
+
+    res.send(newProduct);
+})
 app.listen(port, () => {
         console.log(`listening at ${url}:${port}`)
     }
